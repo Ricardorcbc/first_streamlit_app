eafcc7cecad4317eae40c6a91227f231f571dcf1
Ricardorcbc <126289391+Ricardorcbc@users.noreply.github.com>
2023-03-07 12:37:56 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.error importURLError
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
